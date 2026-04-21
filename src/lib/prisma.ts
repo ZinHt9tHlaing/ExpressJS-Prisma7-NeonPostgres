4e2f8c770354edf2443300e0a699940f1a8fdc3a
ZinHt9tHlaing <phyoearkar267@gmail.com>
2026-04-21 22:50:08 +0630

    refactor and setup prisma orm

---
+++ src/lib/prisma.ts
@@ -0,0 +1,12 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client";
+
+const databaseUrl = process.env.DATABASE_URL;
+
+const adapter = new PrismaPg({ connectionString: databaseUrl });
+
+const globalPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalPrisma.prisma = prisma;
